1db5752eed4bb26c90e2fef4729ab570ca6f694c
Les Chinyanga <les.chinyanga@africacode.academy>
2022-01-20 23:56:16 +0200

    Security packages added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const app = express();
 
@@ -11,7 +15,18 @@ const entriesRouter = require('./routes/entryRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticationMiddleware = require('./middleware/authentication');
-const res = require('express/lib/response');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use(express.static('./public'));
 app.use(express.json());
