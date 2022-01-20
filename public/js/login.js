f4c2003d1f3476b805f57d6121b9ca31d84c72ff
Les Chinyanga <les.chinyanga@africacode.academy>
2022-01-20 14:57:42 +0200

    Add frontend validation for index.html

--- public/js/login.js
+++ public/js/login.js
@@ -3,11 +3,44 @@ const email = document.querySelector('#inputEmail');
 const password = document.querySelector('#inputPassword');
 const loginBtn = document.querySelector('#btnLogin');
 
+const triggerError = (input, message) => {
+  const formControl = input.parentElement; // grabs the parent div of the current input
+  const small = formControl.querySelector('small'); // for the error text
+
+  formControl.className = 'form-control error';
+
+  small.innerText = message;
+};
+
+const triggerSuccess = (input) => {
+  const formControl = input.parentElement; // grabs the parent div of the current input
+
+  formControl.className = 'form-control success';
+};
+
+const checkInputs = () => {
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '') {
+    triggerError(email, 'Email cannot be empty');
+  }
+  {
+    triggerSuccess(email);
+  }
+
+  if (passwordValue === '') {
+    triggerError(password, 'Password cannot be empty');
+  }
+  {
+    triggerSuccess(password);
+  }
+};
+
 window.onload = function () {
   const user = localStorage.getItem('diary_user');
   const token = localStorage.getItem('token');
   if (user || token) {
-    // alert('forbidden');
     window.location.replace('/dashboard.html');
   }
 };
@@ -16,26 +49,43 @@ window.onload = function () {
 loginForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
+  checkInputs();
+
   try {
     const emailValue = email.value;
     const passwordValue = password.value;
 
-    const { data } = await axios.post('/api/v1/auth/login', {
-      email: emailValue,
-      password: passwordValue,
-    });
-    const { user, token } = data;
+    const request = await axios
+      .post('/api/v1/auth/login', {
+        email: emailValue,
+        password: passwordValue,
+      })
+      .catch(function (error) {
+        if (error.response.status === 400) {
+          triggerError(email, 'BAD REQUEST: Check fields for empty values');
+          triggerError(password, 'BAD REQUEST: Check fields for empty values');
+          console.log(error.response.status);
+        }
+
+        if (error.response.status === 401) {
+          triggerError(email, 'Invalid credentials, check and try again');
+          triggerError(password, 'Invalid credentials, check and try again');
+        }
+      });
+
+    const { user, token } = request.data;
     console.log(user, token);
+
+    if (request.status === 200) {
+      triggerSuccess(email);
+      triggerSuccess(password);
+    }
 
     // PLACES USER AND THEIR TOKEN IN LOCAL STORAGE
     if (user) {
       localStorage.setItem('diary_user', user.name);
       localStorage.setItem('token', token);
       window.location.replace('/dashboard.html');
-    } else {
-      alert('Credentials must be wrong'); // TODO: GOING TO CHANGE THIS TO SOMETHING MORE REASONABLE
     }
-  } catch (error) {
-    console.error(error);
-  }
+  } catch (error) {}
 });
